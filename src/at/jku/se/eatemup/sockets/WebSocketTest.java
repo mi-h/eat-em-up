774c4b74c13c6e37adde99e2e80310a073cd4e24
markus <marce@marce.at>
2013-11-01 13:59:20 +0100

    test socket json

--- src/at/jku/se/eatemup/sockets/WebSocketTest.java
+++ src/at/jku/se/eatemup/sockets/WebSocketTest.java
@@ -1,6 +1,8 @@
 package at.jku.se.eatemup.sockets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
@@ -8,31 +10,46 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import at.jku.se.eatemup.core.json.CastType;
+import at.jku.se.eatemup.core.json.JsonTool;
+import at.jku.se.eatemup.core.json.MessageType;
+import at.jku.se.eatemup.core.json.TempMessageContainer;
+import at.jku.se.eatemup.core.json.messages.GameEndMessage;
+
 @ServerEndpoint("/websocketTest")
 public class WebSocketTest {
 
   @OnMessage
   public void onMessage(String message, Session session) 
     throws IOException, InterruptedException {
+	  
+	  GameEndMessage msg = new GameEndMessage();
+		msg.castType = CastType.Broadcast;
+		msg.teamRedWin = true;
+		HashMap<String,Object> map1 = new HashMap<>();
+		map1.put("test", 1);
+		map1.put("bool", true);
+		map1.put("string", "hello world");
+		HashMap<String,Object> map2 = new HashMap<>();
+		map2.put("test2", 2);
+		map2.put("bool2", false);
+		map2.put("string2", "bye world");
+		msg.playerResults = new ArrayList<>();
+		msg.playerResults.add(map1);
+		msg.playerResults.add(map2);
+		String serial1 = JsonTool.SerializeMessage(msg);
+		TempMessageContainer temp = new TempMessageContainer();
+      temp.message = serial1;
+      temp.type = MessageType.GameEnd;
+      String serial2 = JsonTool.SerializeTempMessageContainer(temp);
   
     // Print the client message for testing purposes
     System.out.println("Received: " + message);
   
     // Send the first message to the client
-    session.getBasicRemote().sendText("This is the first server message, your session-id is: "+session.getId());
-  
-    // Send 3 messages to the client every 5 seconds
-    int sentMessages = 0;
-    while(sentMessages < 3){
-      Thread.sleep(5000);
-      session.getBasicRemote().
-        sendText("This is an intermediate server message. Count: " 
-          + sentMessages);
-      sentMessages++;
-    }
-  
+    session.getBasicRemote().sendText("This is the first server message, your session-id is: "+session.getId());  
     // Send a final message to the client
-    session.getBasicRemote().sendText("This is the last server message");
+    session.getBasicRemote().sendText(serial2);
   }
   
   @OnOpen
