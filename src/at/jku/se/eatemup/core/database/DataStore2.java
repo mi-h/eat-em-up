45d19ddbd7d9935ee0e0d79d698b62c063860912
markus <marce@marce.at>
2013-11-04 22:23:48 +0100

    added files

---
+++ src/at/jku/se/eatemup/core/database/DataStore2.java
@@ -0,0 +1,214 @@
+package at.jku.se.eatemup.core.database;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import at.jku.se.eatemup.core.logging.LogEntry;
+import at.jku.se.eatemup.core.logging.Logger;
+import at.jku.se.eatemup.core.model.Account;
+import at.jku.se.eatemup.core.model.GoodiePoint;
+import at.jku.se.eatemup.core.model.Location;
+import at.jku.se.eatemup.core.model.Position;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+public class DataStore2{
+
+	private String connection;
+	private String url;
+	private String dbName;
+	private String dbUser;
+	private String dbPassword;
+	private ConnectionSource connectionSource;
+	private Dao<Account,String> accountDao;
+	private Dao<LogEntry,String> logDao;
+	private Dao<Position,Integer> positionDao;
+
+	public DataStore2() {
+		this.url = "camellia.arvixe.com:1433";
+		this.dbName = "sepr";
+		this.dbUser = "sepruser";
+		this.dbPassword = "seprpass";
+		connection = "jdbc:sqlserver://" + this.url + ";databaseName="
+				+ this.dbName + ";user=" + this.dbUser + ";password="
+				+ this.dbPassword + ";";
+		try {
+			connectionSource =
+			        new JdbcConnectionSource(connection);
+		} catch (SQLException e) {
+			Logger.log("jdbc connection failed."+Logger.stringifyException(e));
+		}
+		try {
+			accountDao = DaoManager.createDao(connectionSource, Account.class);
+		} catch (SQLException e) {
+			Logger.log("account dao creation failed."+Logger.stringifyException(e));
+		}
+		try {
+			logDao = DaoManager.createDao(connectionSource, LogEntry.class);
+		} catch (SQLException e) {
+			Logger.log("log dao creation failed."+Logger.stringifyException(e));
+		}
+		try {
+			positionDao = DaoManager.createDao(connectionSource, Position.class);
+		} catch (SQLException e) {
+			Logger.log("position dao creation failed."+Logger.stringifyException(e));
+		}		
+	}
+	
+	public void createTables(){
+		try {
+			TableUtils.createTableIfNotExists(connectionSource, Account.class);
+		} catch (SQLException e) {
+			Logger.log("account table creation failed."+Logger.stringifyException(e));
+		}
+		try {
+			TableUtils.createTableIfNotExists(connectionSource, LogEntry.class);
+		} catch (SQLException e) {
+			Logger.log("log table creation failed."+Logger.stringifyException(e));
+		}
+		try {
+			TableUtils.createTable(connectionSource, Position.class);
+		} catch (SQLException e) {
+			Logger.log("position table creation failed."+Logger.stringifyException(e));
+		}
+	}
+	
+	public void closeConnection(){
+		connectionSource.closeQuietly();
+	}
+	
+	public void saveLogEntry(LogEntry e){
+		try {
+			logDao.create(e);
+		} catch (SQLException e1) {
+			System.out.println("saving logentry failed");
+		}
+	}
+	
+	public ArrayList<LogEntry> getLogEntries(Date start, Date end){
+		QueryBuilder<LogEntry, String> queryBuilder = logDao.queryBuilder();
+		try {
+			queryBuilder.where().le("created", end);
+		} catch (SQLException e1) {
+			Logger.log("adding lowerthanequal to logentry query failed."+Logger.stringifyException(e1));
+		}
+		try {
+			queryBuilder.where().ge("created", start);
+		} catch (SQLException e1) {
+			Logger.log("adding greaterthanequal to logentry query failed."+Logger.stringifyException(e1));
+		}
+		PreparedQuery<LogEntry> preparedQuery = null;
+		try {
+			preparedQuery = queryBuilder.prepare();
+		} catch (SQLException e) {
+			Logger.log("preparing logentry query failed."+Logger.stringifyException(e));
+		}
+		try {
+			return new ArrayList<LogEntry>(logDao.query(preparedQuery));
+		} catch (SQLException e) {
+			Logger.log("logentry query failed."+Logger.stringifyException(e));
+		}
+		return new ArrayList<LogEntry>();
+	}
+	
+	public Account getAccountByUsername(String username){
+		try {
+			Account temp = accountDao.queryForId(username);
+			return temp;
+		} catch (SQLException e) {
+			Logger.log("account retrieval failed for "+username+"."+Logger.stringifyException(e));
+			return null;
+		}
+	}
+	
+	public ArrayList<Account> getAccountsByUsernames(ArrayList<String> usernames){
+		ArrayList<Account> list = new ArrayList<>();
+		for (String u : usernames){
+			Account tmp = getAccountByUsername(u);
+			if (tmp != null){
+				list.add(tmp);
+			}
+		}
+		return list;
+	}
+	
+	public ArrayList<Account> getHighscore(int topX){
+		QueryBuilder<Account, String> queryBuilder = accountDao.queryBuilder();
+		queryBuilder.orderBy("points", false);
+		queryBuilder.limit(new Long(topX));
+		PreparedQuery<Account> preparedQuery = null;
+		try {
+			preparedQuery = queryBuilder.prepare();
+		} catch (SQLException e) {
+			Logger.log("preparing highscore query failed."+Logger.stringifyException(e));
+		}
+		try {
+			return new ArrayList<Account>(accountDao.query(preparedQuery));
+		} catch (SQLException e) {
+			Logger.log("highscore query failed."+Logger.stringifyException(e));
+		}
+		return new ArrayList<Account>();
+	}
+	
+	public void addUserPoints(String username, int points){
+		Account tmp = getAccountByUsername(username);
+		if (tmp != null){
+			tmp.addPoints(points);
+			try {
+				accountDao.createOrUpdate(tmp);
+			} catch (SQLException e) {
+				Logger.log("account point update failed for "+username+"."+Logger.stringifyException(e));
+			}
+		}
+	}
+	
+	public String getUserPassword(String username){
+		Account tmp = getAccountByUsername(username);
+		if (tmp != null){
+			return tmp.getName();
+		}
+		return null;
+	}
+	
+	public ArrayList<GoodiePoint> getGoodiePoints(){
+		ArrayList<GoodiePoint> ret = new ArrayList<GoodiePoint>();
+		try {
+			List<Position> list = positionDao.queryForAll();
+			for (Position pos : list){
+				GoodiePoint tmp = new GoodiePoint();
+				tmp.setPosition(pos);
+				ret.add(tmp);
+			}
+			return ret;
+		} catch (SQLException e) {
+			Logger.log("retrieve goodie point positions failed."+Logger.stringifyException(e));
+			return ret;
+		}
+	}
+
+	public void addAccount(Account acc) {
+		try {
+			accountDao.createIfNotExists(acc);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void addGoodiePosition(Position position){
+		try {
+			positionDao.createIfNotExists(position);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/at/jku/se/eatemup/servlets/DbTest.java
@@ -0,0 +1,62 @@
+package at.jku.se.eatemup.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import at.jku.se.eatemup.core.database.DataStore2;
+import at.jku.se.eatemup.core.model.Account;
+import at.jku.se.eatemup.core.model.GoodiePoint;
+import at.jku.se.eatemup.core.model.Position;
+
+/**
+ * Servlet implementation class DbTest
+ */
+@WebServlet("/DbTest")
+public class DbTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DbTest() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DataStore2 ds = new DataStore2();
+		//ds.createTables();
+		//Account acc = new Account();
+		//acc.setName("tester");
+		//acc.setPassword("password");
+		//ds.addAccount(acc);
+		/*
+		Account acc = ds.getAccountByUsername("tester");
+		ArrayList<Account> high = ds.getHighscore(100);
+		String password = ds.getUserPassword("tester");
+		Position pos = new Position();
+		pos.setLatitude(14.61644);
+		pos.setLongitude(43.71461);
+		ds.addGoodiePosition(pos);
+		ArrayList<GoodiePoint> points = ds.getGoodiePoints();
+		ds.addUserPoints("tester", 55);
+		*/
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
